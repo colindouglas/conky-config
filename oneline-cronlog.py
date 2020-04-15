bc6b88d0cf69fe2edf330f7187bc8b96905b30e7
Colin Douglas <colindouglas@gmail.com>
2020-04-15 15:50:52 -0300

    Initial commit

---
+++ oneline-cronlog.py
@@ -0,0 +1,84 @@
+import re
+from datetime import datetime
+import sys
+
+# Script to print pretty versions of the interesting parts of the cron log
+# Mainly built for conky integration
+# It's called oneline-croglog but actually it prints two lines now
+# Don't @ me
+
+
+PATH = '/var/log/cron.log'  # Path to CRON log
+INTERESTING_USERS = ['colin']  # List of users to monitor
+
+# Max length of the output string
+try:
+    MAXLEN = int(sys.argv[1])  # Fix argument is line length
+except ValueError:
+    MAXLEN = 75  # Otherwise make it 75
+
+patterns = ['(' + user + ').*(CMD)' for user in INTERESTING_USERS]
+pattern = "|".join(patterns)
+
+kept = []
+
+# Open the file, read as a list and strip the whitespace/newlines
+# Only keep the lines that have a match to USERS
+with open(PATH, "r") as log:
+    for line in log:
+        if re.search(pattern, line):
+            kept.append(line.rstrip())
+
+# If the cronlog is empty, check the recently cached log at cronlog.1
+if not len(kept):
+    with open(PATH + ".1", "r") as log:
+        for line in log:
+            if re.search(pattern, line):
+                kept.append(line.rstrip())
+
+
+# Split each line on space or () or /
+cleaned = []
+for line in kept:
+    script_info = tuple(re.split("[ ()/]", line))
+    cleaned.append(script_info)
+
+# Clean it up and format it
+out = []
+for line in cleaned:
+    try:
+        line = [x for x in line if x != '']
+        month, day, time, _, _, user, _, *path = line
+        year = datetime.now().year  # CRON log doesn't keep the year but it's safe to assume it's this year
+        dt = "{0}-{1}-{2} {3}".format(year, month, day, time)
+        dt = datetime.strptime(dt, '%Y-%b-%d %H:%M:%S')
+        time_out = dt.strftime('%a %H:%M')  # Print as Dow HH:MM (Wed, 03:01)
+        out.append('{0} - {1}'.format(time_out, path[-1]))
+    except ValueError:
+        next()
+
+out.reverse()
+
+# Print as many entries as possible so that the string is less than 75 characters
+# Don't truncate the string, though!
+i = 0
+while len(" | ".join(out[0:i])) < MAXLEN:
+    line_one = out[0:i]
+    i += 1
+    if i > len(out):
+        break
+
+j = i
+while len(" | ".join(out[i:j])) < MAXLEN:
+    line_two = out[i:j]
+    j += 1
+    if i > len(out):
+        break
+
+pad_width = max(len(line_one[0]), len(line_two[0]))
+
+line_one = [x.ljust(pad_width) for x in line_one]
+line_two = [x.ljust(pad_width) for x in line_two]
+
+print("  " + " | ".join(line_one))
+print("  " + " | ".join(line_two))
---
+++ plex-now-playing.py
@@ -0,0 +1,109 @@
+import os
+import json
+import requests
+import re
+from datetime import datetime
+from xml.etree import ElementTree as ET
+import keyring
+
+VERSION = "0.1.0"
+WORKING_DIR = '~/scripts/'  # Working directory (ending with /) where script saves tokens
+PLEX_SERVER = 'http://127.0.0.1:32400'  # Address of Plex server to query
+USERNAME = ''  # Plex username to login, only required on first run
+PASSWORD = ''  # Plex password to login, after first run, stored in keyring
+
+# The location of the file that stores Plex token
+token_path = os.path.expanduser(WORKING_DIR) + '.plextoken'
+
+# Check if there's a token saved
+token = None
+username = None
+if os.path.exists(token_path):
+    with open(token_path) as file:
+        username, token = file.readline().split(',')
+    token_last_update = os.path.getmtime(token_path)
+    token_last_update = datetime.fromtimestamp(token_last_update)
+    if (datetime.now() - token_last_update).days >= 1:
+        token = None
+
+# Update the token if there wasn't one saved, or it's older than a day
+if not token:
+    username = username or USERNAME
+    password = keyring.get_password('Plex-Now-Playing', username) or PASSWORD
+    keyring.set_password('Plex-Now-Playing', username, password)
+    token_request = requests.post(
+        url='https://plex.tv/users/sign_in.json',
+        data='user%5Blogin%5D={u}&user%5Bpassword%5D={p}'.format(u=username, p=password),
+        headers={
+            'X-Plex-Client-Identifier': 'Plex-Now-Playing',
+            'X-Plex-Product': 'Plex-Now-Playing',
+            'X-Plex-Version': VERSION
+        }
+    )
+    if token_request.status_code < 300:
+        token = json.loads(token_request.text)['user']['authToken']
+        with open(token_path, 'w') as file:
+            file.write('{0},{1}'.format(username, token))
+    else:
+        print('Authentication problem')
+        exit()
+
+# Ask the local Plex server for the current sessions
+now_playing = requests.get(
+        url=PLEX_SERVER + '/status/sessions',
+        headers={'X-Plex-Token': token}
+    )
+streams_xml = ET.fromstring(now_playing.text)  # Parse the XML returned by Plex
+
+# For each stream, print an informative line about the stream
+if len(streams_xml):  # Is len() here necessary?
+    for stream in streams_xml:
+
+        # Return the user watching the stream.
+        user_full = str(stream.find('User').get('title', default='Unknown User'))
+        user = re.search('[^@]+', user_full)
+        if user is not None:
+            user = user.group(0)
+        else:
+            user = user_full
+
+        # Return either the time a transcode started _or_ the current time as a string
+        start_time = stream.get('lastViewedAt', default=datetime.now())
+        if type(start_time) is str:
+            start_time = datetime.fromtimestamp(int(start_time))
+        start_time = start_time.strftime('%a %H:%M')  # Mon HH:MM
+
+        # The regex returns None if there's no match, None.group() is AttributeError
+        season = re.search('[0-9]+', str(stream.get('parentTitle')))
+        if season is not None:
+            season = season.group(0)
+
+        # Truncate the series name to make it shorter
+        series_name_long = stream.get('grandparentTitle', default='Unknown Series')
+        series_name = ' '.join(series_name_long.split(' ')[:5])  # Only first five words
+
+        # Define how each type of stream is displayed
+        stream_formats = dict()
+        stream_formats['episode'] = '{start}, {user} // {series} - S{season}E{episode} - {tv_name}'
+        stream_formats['movie'] = '{start}, {user} // {movie_title} ({year})'
+        stream_formats['track'] = '{start}, {user} // {track_artist} - {track_title}'
+        stream_formats.setdefault('{start}, {user} // Unknown Stream')
+
+        # Apply formatting to stream data
+        display_str = stream_formats[stream.get('type')].format(
+            start=start_time,
+            movie_title=stream.get('title', default='Unknown Movie'),
+            user=user,
+            series=series_name,
+            season=season,
+            episode=stream.get('index', default=''),
+            tv_name=stream.get('title', default='Unknown Episode'),
+            year=stream.get('originallyAvailableAt').split('-')[0],
+            track_title=stream.get('title', default='Unknown Song'),
+            track_artist=stream.get('grandparentTitle', default='Unknown Artist'),
+            track_album=stream.get('parentTitle', default='Unknown Album'),
+        )
+
+        print("  " + display_str[:75])  # Truncate display at 75 characters
+else:
+    print('Nothing')
